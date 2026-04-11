4d631a896c2d189adb50d3b161010f6349a04f83
HamzaOubada2 <oubham220@gmail.com>
2026-04-11 12:59:33 +0100

    Exercice

--- lists/SearchInsertPosition.java
+++ lists/SearchInsertPosition.java
@@ -20,7 +20,7 @@ public class SearchInsertPosition {
                  * 
                  * REsult
                  * [2,3,4,5,9]
-                 *     ↑ index 1
+                 * ↑ index 1
                  * 
                  */
             }
@@ -29,3 +29,26 @@ public class SearchInsertPosition {
         return nums.length;
     }
 }
+
+/*
+ * Given a sorted array of distinct integers and a target value, return the
+ * index if the target is found. If not, return the index where it would be if
+ * it were inserted in order.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ * Example 2:
+ * 
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ * Example 3:
+ * 
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ */
