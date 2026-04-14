7c9291de72febcccbd51e53c115299e2cc236bd7
HamzaOubada2 <oubham220@gmail.com>
2026-04-14 13:00:59 +0100

    add problems to code

---
+++ lists/ParkingSpeeed.java
@@ -0,0 +1,18 @@
+public class ParkingSpeeed {
+
+}
+
+/*
+ * Next problem! 🎯
+ * 
+ * You work at a parking 🚗
+ * You have a list of car speeds
+ * Find how many cars are speeding (speed > 120)
+ * And print their speeds!
+ * 
+ * javaInput: [90, 130, 110, 145, 100, 125, 80]
+ * limit = 120
+ * Output:
+ * 3 cars are speeding!
+ * Speeds: 130, 145, 125
+ */
--- lists/School.java
+++ lists/School.java
@@ -4,11 +4,22 @@ public class School {
 
         int count = 0;
 
-        for (int i = 0; i< grade.length; i++) {
+        for (int i = 0; i < grade.length; i++) {
             if (grade[i] >= 50) {
-                count ++;
+                count++;
             }
         }
-        System.out.println(count+ " Students passed!");
+        System.out.println(count + " Students passed!");
     }
 }
+
+/*
+ * Next problem! 🎯
+ * 
+ * You work at a school 🏫
+ * You have a list of student grades
+ * Count how many students passed (grade >= 50)
+ * 
+ * javaInput: [45, 78, 32, 90, 55, 48, 61]
+ * Output: 4 students passed!
+ */
